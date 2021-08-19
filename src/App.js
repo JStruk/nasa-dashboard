ff0a80eb7567619b39ad3ad6fbcc9cc1959e84da
Justin Struk <strukjustin@gmail.com>
2021-08-19 10:38:32 -0400

    Make sidebar collapsible for mobile

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ import {
 import './App.css';
 import APOD from './Pages/APOD';
 import Sidebar from './components/Sidebar'
+import Media from "react-media";
 
 function App() {
     return (
         <Router>
-            <div className="flex w-full">
-                <div className="h-auto">
-                    <Sidebar/>
+            <div className="flex w-full min-h-screen">
+                <div className="">
+                    <Media queries={ {
+                        small: "(max-width: 599px)",
+                        medium: "(min-width: 600px) and (max-width: 1199px)",
+                        large: "(min-width: 1200px)"
+                    } }>
+                        { matches => (
+                            <>
+                                { matches.small && <Sidebar collapsible={ true } toggle={true}/> }
+                                { matches.medium && <Sidebar/> }
+                                { matches.large && <Sidebar/> }
+                            </>
+                        ) }
+                    </Media>
                 </div>
                 <Switch>
                     <Route path="/poop">this is poop</Route>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 import { AiOutlinePicture } from 'react-icons/ai';
 import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
+import { useState } from "react";
 
-const Sidebar = () => {
+const Sidebar = ({ collapsible = false, toggle }) => {
+    const [collapsed, setCollapsed] = useState(collapsible)
+
     return (
         <>
-            <ProSidebar title="NASA">
-                <h1 className="m-5 text-center font-bold text-white"> NASA Dashboard </h1>
+            <ProSidebar title="NASA" collapsed={ collapsed }>
+                { toggle && <button className="mt-2" onClick={ () => { setCollapsed(!collapsed) } }>X</button>}
+                <Link to="/" className="m-5 text-center font-bold text-white overflow-hidden"> NASA Dashboard </Link>
                 <Menu iconShape="square">
                     <MenuItem icon={ <AiOutlinePicture/> }>Astronomy Pic OTD <Link to="/apod"/> </MenuItem>
                 </Menu>
