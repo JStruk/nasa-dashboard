21ee754cc768317db23612fba34ef85bc350abd8
Justin Struk <justin.struk@ableto.com>
2021-08-31 17:02:34 -0400

    its not working

--- src/Pages/ISSTrackerPage/index.tsx
+++ src/Pages/ISSTrackerPage/index.tsx
@@ -3,45 +3,57 @@ import axios from 'axios';
 import {useCallback, useEffect, useState} from 'react';
 
 const ISSTrackerPage = (): JSX.Element => {
+    const initialMarker: Marker =  {
+        id: 'international-space-satan',
+        color: 'red',
+        coordinates: [0, 0],
+        value: 50,
+    }
     const [issPosition, setIssPosition] = useState<Marker>(
-        {
-            id: 'international-space-satan',
-            color: 'red',
-            coordinates: [0, 0],
-            value: 50,
-        }
+        initialMarker
     );
 
     const [initialCoords, setinitialCoords] = useState<Coordinates | undefined>()
+    const [coordinates, setCoordinates] = useState<Marker[]>([initialMarker])
 
-    const updateIssPosition = useCallback(async () => {
+    const updateIssPosition = async () => {
         const result = await axios.get('http://api.open-notify.org/iss-now.json')
-        setIssPosition({
-            id: 'international-space-satan',
+        const newMarkers: Marker[] = [...coordinates];
+        newMarkers.push({
+            id: 'international-space-satan' + Math.random(),
             color: 'red',
             coordinates: [result.data.iss_position.latitude, result.data.iss_position.longitude],
             value: 50,
         })
 
+        setCoordinates(newMarkers)
+        // eslint-disable-next-line no-console
+        console.log({newMarkers});
+
         if (!initialCoords) {
             setinitialCoords([result.data.iss_position.latitude, result.data.iss_position.longitude])
         }
-    }, [initialCoords])
+    }
 
     useEffect(() => {
         (async () => updateIssPosition())()
     }, [])
 
+    const UPDATEFOOO = async () => {
+        await updateIssPosition()
+        setTimeout(UPDATEFOOO, 5000)
+    }
+
     useEffect(() => {
         if (initialCoords) {
-            setInterval(() => updateIssPosition(), 5000);
+            setTimeout(UPDATEFOOO, 5000);
         }
     }, [initialCoords])
 
     return (
         <div className="w-full h-screen">
             {initialCoords && <ReactGlobe
-                markers={[issPosition]}
+                markers={coordinates}
                 initialCoordinates={initialCoords}
             />}
         </div>
