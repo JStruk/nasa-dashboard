de36ae18a1a1c64f9766a2773f9e67b38625bc41
Justin Struk <justin.struk@ableto.com>
2021-09-01 08:06:56 -0400

    Try to make the markers work

--- src/Pages/ISSTrackerPage/index.tsx
+++ src/Pages/ISSTrackerPage/index.tsx
@@ -1,38 +1,60 @@
-import ReactGlobe, {Marker, Coordinates} from 'react-globe'
+import ReactGlobe, {Coordinates, Marker} from 'react-globe'
 import axios from 'axios';
-import {useCallback, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 
 const ISSTrackerPage = (): JSX.Element => {
-    const initialMarker: Marker =  {
+    const initialMarker: Marker = {
         id: 'international-space-satan',
         color: 'red',
         coordinates: [0, 0],
         value: 50,
     }
-    const [issPosition, setIssPosition] = useState<Marker>(
-        initialMarker
-    );
+    // const [issPosition, setIssPosition] = useState<Marker>(
+    //     initialMarker
+    // );
 
     const [initialCoords, setinitialCoords] = useState<Coordinates | undefined>()
-    const [coordinates, setCoordinates] = useState<Marker[]>([initialMarker])
+    const [issPositionHistory, setIssPositionHistory] = useState<Marker[]>([initialMarker])
+
+    const getNewIssLatLonFromApi = async (): Promise<Coordinates> => {
+        const result = await axios.get('http://api.open-notify.org/iss-now.json')
+        return [result.data.iss_position.latitude, result.data.iss_position.longitude]
+    }
 
     const updateIssPosition = async () => {
-        const result = await axios.get('http://api.open-notify.org/iss-now.json')
-        const newMarkers: Marker[] = [...coordinates];
-        newMarkers.push({
+        const newestPosition = await getNewIssLatLonFromApi()
+        if (!initialCoords) {
+            setinitialCoords(newestPosition)
+        }
+
+        const crapfackulateTheId = (id: string): string => {
+            return (id.length > 50
+                ? id.substr(50 - id.length)
+                : id) + Math.random()
+        }
+
+        let newMarkers: Marker[] = [...issPositionHistory]
+            // de-satan the initial marker:
+            .filter(marker => marker.id !== 'international-space-satan')
+            // age the markers and adjust colour:
+            .map(marker => ({
+                ...marker,
+                value: marker.value - 0.5,
+                color: `rgba(${Math.round(5.1 * marker.value)}, ${Math.round(2.55 * marker.value)}, 0, 1)`,
+            }))
+            // kill the elderly:
+            .filter(marker => marker.value > 0)
+            // give them all new IDs:
+            .map(marker => ({...marker, id: crapfackulateTheId(marker.id)}))
+
+        newMarkers = newMarkers.concat([{
             id: 'international-space-satan' + Math.random(),
             color: 'red',
-            coordinates: [result.data.iss_position.latitude, result.data.iss_position.longitude],
+            coordinates: newestPosition,
             value: 50,
-        })
+        }])
 
-        setCoordinates(newMarkers)
-        // eslint-disable-next-line no-console
-        console.log({newMarkers});
-
-        if (!initialCoords) {
-            setinitialCoords([result.data.iss_position.latitude, result.data.iss_position.longitude])
-        }
+        setIssPositionHistory(newMarkers)
     }
 
     useEffect(() => {
@@ -41,19 +63,20 @@ const ISSTrackerPage = (): JSX.Element => {
 
     const UPDATEFOOO = async () => {
         await updateIssPosition()
-        setTimeout(UPDATEFOOO, 5000)
+        // setTimeout(UPDATEFOOO, 5000)
     }
 
     useEffect(() => {
         if (initialCoords) {
-            setTimeout(UPDATEFOOO, 5000);
+            const fleemBucket = setTimeout(UPDATEFOOO, 5000);
+            return () => clearTimeout(fleemBucket)
         }
-    }, [initialCoords])
+    }, [initialCoords, issPositionHistory])
 
     return (
         <div className="w-full h-screen">
             {initialCoords && <ReactGlobe
-                markers={coordinates}
+                markers={issPositionHistory}
                 initialCoordinates={initialCoords}
             />}
         </div>
