05ed648db16cef7979253e30874671564594eb12
Justin Struk <strukjustin@gmail.com>
2021-09-08 12:16:49 -0400

    add coverage for Card component

--- src/tests/components/Button.test.tsx
+++ src/tests/components/Button.test.tsx
@@ -4,7 +4,7 @@ import Button from "components/Button";
 describe('Button', () => {
     it('should render a button', () => {
         render(<Button onClick={ jest.fn() } id='button123' testId='button-test-id' />);
-        const buttonElement = screen.getByText('button-test-id');
+        const buttonElement = screen.getByTestId('button-test-id');
         expect(buttonElement).toBeInTheDocument();
     });
 })
