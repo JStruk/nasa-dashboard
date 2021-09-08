ae7813f333d462ce5d11fcd3ad79ec67678ff753
Justin Struk <strukjustin@gmail.com>
2021-09-08 12:10:51 -0400

    add test coverage for Button component

---
+++ src/tests/components/Button.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Button from "components/Button";
+
+describe('Button', () => {
+    it('should render a button', () => {
+        render(<Button onClick={ jest.fn() } id='button123' testId='button-test-id' />);
+        const buttonElement = screen.getByText('button-test-id');
+        expect(buttonElement).toBeInTheDocument();
+    });
+})
