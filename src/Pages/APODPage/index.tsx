d5e56dc064e3fc06c47f7a2ab809473cae24b1a8
Justin Struk <justin.struk@ableto.com>
2021-11-04 16:35:14 -0400

    APOD images: add date pickers

--- src/Pages/APODPage/index.tsx
+++ src/Pages/APODPage/index.tsx
@@ -3,18 +3,35 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Card from '../../components/Card'
 import APOD from './types'
-import { css } from "@emotion/react";
-import ClipLoader from "react-spinners/ClipLoader";
 import { PacmanLoader } from "react-spinners";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 
 const APODPage = (): JSX.Element => {
     const [apods, setApods] = useState([]);
     const [count, setCount] = useState<number>(5)
     const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [startDate, setStartDate] = useState<Date | null>(new Date());
+    const [endDate, setEndDate] = useState<Date | null>(new Date());
 
     const fetchApod = async () => {
+        const nasaDate = (date: Date | null): string => date ? date.toISOString().slice(0, 10) : 'beefmolk'
+
+        const getUrl = (count: number, startDate: Date | null, endDate: Date | null) => {
+            const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=BkQerCYLcMl5BzaHg6vsasTgunnXJnO4tPTgehic&thumbs=true`;
+            if (!(startDate || endDate)) {
+                return `${baseUrl}&count=${count}`
+            }
+
+            if (startDate && endDate) {
+                return `${baseUrl}&start_date=${nasaDate(startDate)}&end_date=${nasaDate(endDate)}`
+            }
+
+            return `${baseUrl}&date=${nasaDate(startDate || endDate)}`
+        }
         setIsLoading(true)
-        const { data: fetchedApods } = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=BkQerCYLcMl5BzaHg6vsasTgunnXJnO4tPTgehic&thumbs=true&count=${count}`);
+
+        const { data: fetchedApods } = await axios.get(getUrl(count, startDate, endDate))
         setApods(fetchedApods)
         setIsLoading(false)
     }
@@ -27,7 +44,7 @@ const APODPage = (): JSX.Element => {
 
     return (
         <div data-testid='apod-page-div'>
-            <div className="ml-6">
+            <div className="ml-6 bg-gray-400 rounded shadow-2xl p-2">
                 <label>
                     Number of images
                     <input
@@ -43,6 +60,33 @@ const APODPage = (): JSX.Element => {
                 <button className="m-6 p-3 rounded border border-blue-300" onClick={() => fetchApod()}>Click to Refresh
                     images
                 </button>
+                <div className="flex">
+                    <span>Start Date:</span>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date: Date) => setStartDate(date)}
+                        customInput={
+                            <button
+                                className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                onClick={() => {
+                                    return {}
+                                }}> {startDate?.toDateString()} </button>}
+                    />
+                </div>
+                <div className="flex mt-4">
+                    <span>End Date:</span>
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date: Date) => setEndDate(date)}
+                        customInput={
+                            <button
+                                className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                onClick={() => {
+                                    return {}
+                                }}> {endDate?.toDateString()} </button>}
+                    />
+                </div>
+
             </div>
             {isLoading &&
             <div className="flex flex-col justify-center h-screen items-center">
